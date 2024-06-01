424fcd798b2a1996fc984182dea11ad4cedbfe11
hanbuck30 <hanbuck30@naver.com>
2024-06-01 16:04:08 +0900

    new file:   ESN_example.ipynb 	new file:   ESN_py/ESN.py 	new file:   ESN_py/__init__.py 	new file:   ESN_py/config.py 	new file:   ESN_py/utils/__pycache__/func.cpython-38.pyc 	new file:   ESN_py/utils/__pycache__/setting.cpython-38.pyc 	new file:   ESN_py/utils/__pycache__/sparseLIB.cpython-38.pyc 	new file:   ESN_py/utils/func.py 	new file:   ESN_py/utils/setting.py 	new file:   ESN_py/utils/sparseLIB.py 	new file:   MackeyGlass_t17.txt

---
+++ ESN_py/utils/setting.py
@@ -0,0 +1,58 @@
+import torch
+import numpy as np
+import random
+
+def get_loss_func(args):
+    if args.Loss_function == 'mse':
+        return torch.nn.MSELoss()
+    
+    elif args.Loss_function == 'cross_entropy':
+        return torch.nn.CrossEntropyLoss()
+    
+    elif args.Loss_function == 'log_loss':
+        return torch.nn.NLLLoss()
+
+def get_torch_func(args): # 파라미터 inter_unit 데이터 타입을 맞춰주기 위해서 만든 함수입니다.
+    if args.inter_unit == 'tanh':
+        return torch.tanh
+    elif args.inter_unit == 'sin':
+        return torch.sin
+    elif args.inter_unit == 'relu':
+        return  torch.relu
+    elif args.inter_unit == 'leaky_relu':
+        return  torch.leaky_relu
+    elif args.inter_unit == 'sigmoid':
+        return  torch.sigmoid
+    else:
+        raise ValueError('Invalid function name')
+
+def set_common(self, args):
+    self.resSize = int(args.resSize)
+    self.n_feature = np.around(args.n_feature,5)
+    self.damping = np.around(args.damping,5)
+    self.output_dim = args.output_dim
+    self.spectral_radius = np.around(args.spectral_radius,5)
+    self.weight_scaling = np.around(args.weight_scaling,5)
+    self.input_scaling = np.around(args.input_scaling,5)
+    self.l_a = args.l_a
+    self.sparsity = np.around(args.sparsity,5)
+    self.Loss_function = get_loss_func(args)
+    self.Test = args.Test
+  
+    inter_unit1 = get_torch_func(args)
+    self.inter_unit = inter_unit1
+    
+def set_gd(self, args):
+    self.learning_rate = args.learning_rate
+    self.epoch = args.epoch
+    self.l2_lambda = args.l2_lambda # L2 규제 계수
+
+def set_random_seed(seed_val):
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    return
